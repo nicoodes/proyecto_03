4a424576a1739a16be506aa27c28113e5bf81145
nicoodes <nicoodes@hotmail.com>
2017-06-27 15:23:37 +1000

    js added to global slider

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -1,3 +1,6 @@
+/////////////////
+//Google maps ///
+/////////////////
 var map;
 var marker;
 function initMap() {
@@ -12,9 +15,9 @@ function initMap() {
   });
 }
 
-
-
-
+///////////////
+// Menu bar ///
+///////////////
 var menu = document.getElementById("menu");
 var mainNav = document.getElementById("mainNav");
 
@@ -34,6 +37,7 @@ function menuShow () {
 menu.addEventListener("click", menuShow);
 
 
+// This will close the menu bar to avoid being left open
 
 function resize () {
 	// console.log(window.innerWidth);
@@ -44,3 +48,75 @@ function resize () {
 }
 
 window.addEventListener("resize", resize);
+
+
+///////////////////
+// Slider Global //
+///////////////////
+images = [
+    "img/globe.png",
+    "img/globe2.png",
+    "img/globe3.png",
+    "img/globe4.png",
+    "img/globe5.png"
+];
+
+globeCount = 0;
+
+globeImage = document.getElementById("globeSlider");
+globeNext = document.getElementById("globeNext");
+globePrev = document.getElementById("globePrev");
+globeP = document.getElementById("globeP");
+globeH = document.getElementById("globeH");
+
+function goNext() {
+
+  if (globeCount < images.length - 1) {
+    
+    globeCount++;
+    globeImage.src = images[globeCount];
+    globeP.innerHTML = "0"+(globeCount + 1)+" / 05";
+    globeH.innerHTML = "GLOBAL No "+(globeCount + 1);
+    document.getElementsByClassName("circle__slider--active")[0].className = "circle circle__slider";
+    document.getElementById("circle"+(globeCount+1)).className = "circle circle__slider--active";
+    // console.log(document.getElementsByClassName("circle__slider")[globeCount]);
+
+  } else {
+    globeCount = 0;
+    globeImage.src = images[globeCount];
+    globeP.innerHTML = "0"+(globeCount + 1)+" / 05";
+    globeH.innerHTML = "GLOBAL No "+(globeCount + 1);
+    document.getElementsByClassName("circle__slider--active")[0].className = "circle circle__slider";
+    document.getElementById("circle"+(globeCount+1)).className = "circle circle__slider--active";
+    // console.log(document.getElementsByClassName("circle__slider")[globeCount]);
+  }
+
+  // console.log(globeCount);
+}
+
+function goPrev() {
+
+  if (globeCount > 0) {
+    
+    globeCount--;
+    globeImage.src = images[globeCount];
+    globeP.innerHTML = "0"+(globeCount + 1)+" / 05";
+    globeH.innerHTML = "GLOBAL No "+(globeCount + 1);
+    document.getElementsByClassName("circle__slider--active")[0].className = "circle circle__slider";
+    document.getElementById("circle"+(globeCount+1)).className = "circle circle__slider--active";
+
+  } else {
+
+    globeCount = images.length - 1;
+    globeImage.src = images[globeCount];
+    globeP.innerHTML = "0"+(globeCount + 1)+" / 05";
+    globeH.innerHTML = "GLOBAL No "+(globeCount + 1);
+    document.getElementsByClassName("circle__slider--active")[0].className = "circle circle__slider";
+    document.getElementById("circle"+(globeCount+1)).className = "circle circle__slider--active";
+  }
+  // console.log(globeCount);
+}
+
+
+globeNext.addEventListener("click", goNext);
+globePrev.addEventListener("click", goPrev);
