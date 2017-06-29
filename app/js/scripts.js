75050ee409c4dbca0df4aff33b78b7c8848ad1de
nicoodes <nicoodes@hotmail.com>
2017-06-30 00:30:15 +1000

    added modal and slider in about-us page

--- app/js/scripts.js
+++ app/js/scripts.js
@@ -50,135 +50,25 @@ function resize () {
 window.addEventListener("resize", resize);
 
 
-///////////////////
-// Slider Global //
-///////////////////
-var images = [
-    "img/globe.png",
-    "img/globe2.png",
-    "img/globe3.png",
-    "img/globe4.png",
-    "img/globe5.png"
-];
+////////////
+// MODAL ///
+////////////
 
-var globeCount = 0;
+var modal = document.getElementById("modal");
+var close = document.getElementById("close-modal");
+var open = document.getElementById("open-modal");
+var modalBg = document.getElementById("modalBg");
 
-var globeImage = document.getElementById("globeSlider");
-var globeNext = document.getElementById("globeNext");
-var globePrev = document.getElementById("globePrev");
-var globeP = document.getElementById("globeP");
-var globeH = document.getElementById("globeH");
 
-function goNext() {
-
-  if (globeCount < images.length - 1) {
-    
-    globeCount++;
-    globeImage.src = images[globeCount];
-    globeP.innerHTML = "0"+(globeCount + 1)+" / 05";
-    globeH.innerHTML = "GLOBAL No "+(globeCount + 1);
-    document.getElementsByClassName("circle__slider--active")[0].className = "circle circle__slider";
-    document.getElementById("circle"+(globeCount+1)).className = "circle circle__slider--active";
-    // console.log(document.getElementsByClassName("circle__slider")[globeCount]);
-
-  } else {
-    globeCount = 0;
-    globeImage.src = images[globeCount];
-    globeP.innerHTML = "0"+(globeCount + 1)+" / 05";
-    globeH.innerHTML = "GLOBAL No "+(globeCount + 1);
-    document.getElementsByClassName("circle__slider--active")[0].className = "circle circle__slider";
-    document.getElementById("circle"+(globeCount+1)).className = "circle circle__slider--active";
-    // console.log(document.getElementsByClassName("circle__slider")[globeCount]);
-  }
-
-  // console.log(globeCount);
+function openModal() {
+  modal.classList.add("modal-on");
+  
 }
 
-function goPrev() {
-
-  if (globeCount > 0) {
-    
-    globeCount--;
-    globeImage.src = images[globeCount];
-    globeP.innerHTML = "0"+(globeCount + 1)+" / 05";
-    globeH.innerHTML = "GLOBAL No "+(globeCount + 1);
-    document.getElementsByClassName("circle__slider--active")[0].className = "circle circle__slider";
-    document.getElementById("circle"+(globeCount+1)).className = "circle circle__slider--active";
-
-  } else {
-
-    globeCount = images.length - 1;
-    globeImage.src = images[globeCount];
-    globeP.innerHTML = "0"+(globeCount + 1)+" / 05";
-    globeH.innerHTML = "GLOBAL No "+(globeCount + 1);
-    document.getElementsByClassName("circle__slider--active")[0].className = "circle circle__slider";
-    document.getElementById("circle"+(globeCount+1)).className = "circle circle__slider--active";
-  }
-  // console.log(globeCount);
+function closeModal() {
+  modal.classList.remove("modal-on");
 }
 
-
-globeNext.addEventListener("click", goNext);
-globePrev.addEventListener("click", goPrev);
-
-
-
-/////////////////////////
-// Slider Testimonials //
-/////////////////////////
-
-
-var opinionCount = 0;
-var opinionOn = document.getElementsByClassName("opinion-on");
-var opinionP = document.getElementById("opinionP");
-
-function goNextOpinion() {
-
-  if (opinionCount < 4) {
-    
-    opinionCount++;
-    opinionP.innerHTML = "0"+(opinionCount + 1)+" / 05";
-    document.getElementsByClassName("circle__slider2--active")[0].className = "circle circle__slider2";
-    document.getElementById("circle0"+(opinionCount+1)).className = "circle circle__slider2--active";
-    // console.log(document.getElementsByClassName("circle__slider2")[opinionCount]);
-   // console.log("circle0"+(opinionCount+1));
-  } else {
-      opinionCount = 0;
-      opinionP.innerHTML = "0"+(opinionCount + 1)+" / 05";
-      
-      document.getElementsByClassName("circle__slider2--active")[0].className = "circle circle__slider2";
-      document.getElementById("circle0"+(opinionCount+1)).className = "circle circle__slider2--active";
-    }
-
-  opinionOn[0].className = "opinion";
-  document.getElementById("opinion"+(opinionCount+1)).className = "opinion-on";
-
-}
-
-function goPrevOpinion() {
-
-  if (opinionCount > 0) {
-    
-    opinionCount--;
-    
-    opinionP.innerHTML = "0"+(opinionCount + 1)+" / 05";
-    
-    document.getElementsByClassName("circle__slider2--active")[0].className = "circle circle__slider2";
-    document.getElementById("circle0"+(opinionCount+1)).className = "circle circle__slider2--active";
-
-  } else {
-
-    opinionCount = 4;
-    
-    opinionP.innerHTML = "0"+(opinionCount + 1)+" / 05";
-    
-    document.getElementsByClassName("circle__slider2--active")[0].className = "circle circle__slider2";
-    document.getElementById("circle0"+(opinionCount+1)).className = "circle circle__slider2--active";
-  }
-  opinionOn[0].className = "opinion";
-  document.getElementById("opinion"+(opinionCount+1)).className = "opinion-on";
-}
-
-
-opinionNext.addEventListener("click", goNextOpinion);
-opinionPrev.addEventListener("click", goPrevOpinion);
+open.addEventListener("click", openModal);
+close.addEventListener("click", closeModal);
+modalBg.addEventListener("click", closeModal);
---
+++ app/js/slider-about-us.js
@@ -0,0 +1,95 @@
+///////////////////
+// Slider Global //
+///////////////////
+var images = [
+    "img/medium.jpg",
+    "img/small-1.jpg",
+    "img/small-2.jpg",
+    "img/small-3.jpg",
+    "img/small-4.jpg",
+    "img/small-5.jpg",
+    "img/small-6.jpg"
+];
+
+var text = [
+    "Crazy app design<span>15 Dec 2016</span>",
+    "Teto app design<span>15 Dec 2016</span>",
+    "Pepe app design<span>15 Dec 2016</span>",
+    "Toto app design<span>15 Dec 2016</span>",
+    "Lili app design<span>15 Dec 2016</span>",
+    "Choto app design<span>15 Dec 2016</span>",
+    "Toti app design<span>15 Dec 2016</span>"
+];
+
+
+var portfCount = 0;
+
+var portfImage = document.getElementById("portfSlider");
+var portfNext = document.getElementById("portfNext");
+var portfPrev = document.getElementById("portfPrev");
+var portfP = document.getElementById("portfP");
+var portfH = document.getElementById("portfH");
+
+// slider on modal
+var modalImg = document.getElementById("modalImg");
+var modalNext = document.getElementById("modalNext");
+var modalPrev = document.getElementById("modalPrev");
+
+
+function goNext() {
+
+  if (portfCount < images.length - 1) {
+    
+    portfCount++;
+    portfImage.src = images[portfCount];
+    modalImg.src = portfImage.src;
+    portfP.innerHTML = "0"+(portfCount + 1)+" / 0" + images.length;
+    portfH.innerHTML = text[portfCount];
+    document.getElementsByClassName("circle__slider--active")[0].className = "circle circle__slider";
+    document.getElementById("circle"+(portfCount+1)).className = "circle circle__slider--active";
+    // console.log(document.getElementsByClassName("circle__slider")[portfCount]);
+
+  } else {
+    portfCount = 0;
+    portfImage.src = images[portfCount];
+    modalImg.src = portfImage.src;
+    portfP.innerHTML = "0"+(portfCount + 1)+" / 0" + images.length;
+    portfH.innerHTML = text[portfCount];
+    document.getElementsByClassName("circle__slider--active")[0].className = "circle circle__slider";
+    document.getElementById("circle"+(portfCount+1)).className = "circle circle__slider--active";
+    // console.log(document.getElementsByClassName("circle__slider")[portfCount]);
+  }
+
+  // console.log(portfCount);
+}
+
+function goPrev() {
+
+  if (portfCount > 0) {
+    
+    portfCount--;
+    portfImage.src = images[portfCount];
+    modalImg.src = portfImage.src;
+    portfP.innerHTML = "0"+(portfCount + 1)+" / 0" + images.length;
+    portfH.innerHTML = text[portfCount];
+    document.getElementsByClassName("circle__slider--active")[0].className = "circle circle__slider";
+    document.getElementById("circle"+(portfCount+1)).className = "circle circle__slider--active";
+
+  } else {
+
+    portfCount = images.length - 1;
+    portfImage.src = images[portfCount];
+    modalImg.src = portfImage.src;
+    portfP.innerHTML = "0"+(portfCount + 1)+" / 0" + images.length;
+    portfH.innerHTML = text[portfCount];
+    document.getElementsByClassName("circle__slider--active")[0].className = "circle circle__slider";
+    document.getElementById("circle"+(portfCount+1)).className = "circle circle__slider--active";
+  }
+  // console.log(portfCount);
+}
+
+portfNext.addEventListener("click", goNext);
+portfPrev.addEventListener("click", goPrev);
+modalNext.addEventListener("click", goNext);
+modalPrev.addEventListener("click", goPrev);
+
